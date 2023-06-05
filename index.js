495ce88362207485a3dce8569ea35ba54fe764cb
Md Mahbub Alom <mdmahbuba034@gmail.com>
2023-06-05 14:13:26 +0600

    admin some work finish

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization) {
+    return res.status(401).send({error: true, message: 'unauthorized access' });
+  }
+
+  //bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err) {
+      return res.status(402).send({ error: true, message: 'unauthorized access' });
+    }
+    req.decoded = decoded
+    next();
+  })
+
+
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qud1tkv.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -62,6 +82,24 @@ async function run() {
       res.send(result);
     });
 
+
+    //security layer:
+    //1.verifyJWT
+    //2.email
+    //3.check admin
+    app.get('/users/admin/:email',verifyJWT, async(req,res)=> {
+      const email = req.params.email;
+
+      if(req.decoded.email !== email) {
+        res.send({admin:false})
+      }
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query)
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
     app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) }
@@ -88,13 +126,19 @@ async function run() {
     });
 
     // cart collection apis
-    app.get("/carts", async (req, res) => {
+    app.get("/carts", verifyJWT, async (req, res) => {
       const email = req.query.email;
 
       if (!email) {
         res.send([]);
         return
       }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail) {
+        return res.status(403).send({error:true,message:'forbidden access'})
+      }
+
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
